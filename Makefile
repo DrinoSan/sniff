b60ac4f1db5a1c487ce499135c2529f0f8e3ef14
DrinoSan <s.becirbegovic@gmail.com>
2024-08-25 22:56:43 +0200

    Showing device name, our IP and mask

---
+++ Makefile
@@ -0,0 +1,27 @@
+# Makefile for compiling main.cpp with pcap library
+
+# Compiler
+CC = gcc
+
+# Source files
+SRC = main.cpp
+
+# Output executable
+TARGET = main
+
+# Compiler flags
+CFLAGS = -Wall
+
+# Libraries to link
+LIBS = -lpcap
+
+# Default target to build the project
+all: $(TARGET)
+
+# Rule to build the target
+$(TARGET): $(SRC)
+	$(CC) $(CFLAGS) $(SRC) -o $(TARGET) $(LIBS)
+
+# Clean up build files
+clean:
+	rm -f $(TARGET)
--- main.cpp
+++ main.cpp
@@ -1,25 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
 #include <pcap.h>
 #include <stdio.h>
 
 int main( int argc, char* argv[] )
 {
-    char *     dev, errbuf[ PCAP_ERRBUF_SIZE ];
+    char       errbuf[ PCAP_ERRBUF_SIZE ];
     pcap_if_t* devices = nullptr;
 
-    int ok = pcap_findalldevs( &devices, errbuf );
-    if ( ok != 0 )
+    bpf_u_int32 maskp; /* The netmask of our sniffing device */
+    bpf_u_int32 netp;  /* The IP of our sniffing device */
+
+    int ret = pcap_findalldevs( &devices, errbuf );
+    if ( ret != 0 )
     {
         fprintf( stderr, "Couldn't find default device: %s\n", errbuf );
         return ( 2 );
     }
 
-    pcap_if_t* device = devices;
-    while( device != nullptr )
+    pcap_if_t* dev = devices;
+    if ( pcap_lookupnet( dev->name, &netp, &maskp, errbuf ) == -1 )
     {
-        printf( "Device: %s\n", device->name );
-        device = device->next;
+        fprintf( stderr, "Can't get netmask for device %s\n", dev->name );
+        netp  = 0;
+        maskp = 0;
     }
 
-    pcap_freealldevs(devices);
+    printf( "DEV : %s\n", dev->name );
+
+    /* get the network address in a human readable form */
+    struct in_addr addr;
+    addr.s_addr = netp;
+    char* net   = inet_ntoa( addr );
+
+    if ( net == NULL )
+    {
+        perror( "inet_ntoa" );
+        exit( 1 );
+    }
+
+    printf( "Net : %s\n", net );
+
+    addr.s_addr = maskp;
+    char* mask  = inet_ntoa( addr );
+
+    if ( mask == NULL )
+    {
+        perror( "inet_ntoa" );
+        exit( 1 );
+    }
+
+    printf( "Mask: %s\n", mask );
+
+    pcap_freealldevs( devices );
     return ( 0 );
 }
