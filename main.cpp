e49cc4ccfad5f8fa13448964c08df3e968ee439c
DrinoSan <s.becirbegovic@gmail.com>
2024-08-25 22:30:54 +0200

    Free my device

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@
 int main( int argc, char* argv[] )
 {
     char *     dev, errbuf[ PCAP_ERRBUF_SIZE ];
-    pcap_if_t* devics = nullptr;
+    pcap_if_t* devices = nullptr;
 
-    int ok = pcap_findalldevs( &devics, errbuf );
+    int ok = pcap_findalldevs( &devices, errbuf );
     if ( ok != 0 )
     {
         fprintf( stderr, "Couldn't find default device: %s\n", errbuf );
         return ( 2 );
     }
 
-    pcap_if_t* device = devics;
+    pcap_if_t* device = devices;
     while( device != nullptr )
     {
         printf( "Device: %s\n", device->name );
         device = device->next;
     }
 
+    pcap_freealldevs(devices);
     return ( 0 );
 }
